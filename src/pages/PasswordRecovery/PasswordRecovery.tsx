7d38032e929c62109a98a9f7223603be3e86418c
iForced <iforce@2ch.hk>
2021-11-13 12:24:50 +0300

    add formik to recovery form

--- src/pages/PasswordRecovery/PasswordRecovery.tsx
+++ src/pages/PasswordRecovery/PasswordRecovery.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import s from './PasswordRecovery.module.css'
 import {Button, Card, Input} from 'antd';
 import {NavLink} from "react-router-dom";
+import {useFormik} from "formik";
 
 const PasswordRecovery = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+        },
+        onSubmit: values => {
+            alert(values.email)
+        }
+    })
+
     return (
         <div className={s.wrapper}>
             <Card
@@ -17,9 +28,21 @@ const PasswordRecovery = () => {
             >
                 <h2>IT-Incubator</h2>
                 <p>Forgot your password?</p>
-                <Input placeholder={'Email'}/>
-                <small>Enter your email address and we will send you further instructions</small>
-                <Button shape={'round'} type={'primary'}>Send Instructions</Button>
+                <form onSubmit={formik.handleSubmit} className={s.form}>
+                    <Input
+                        placeholder={'Email'}
+                        name={'email'}
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                    />
+                    <small>Enter your email address and we will send you further instructions</small>
+                    <Button
+                        shape={'round'}
+                        type={'primary'}
+                        htmlType={'submit'}
+                    >Send Instructions
+                    </Button>
+                </form>
                 <p>Did you remember your password?</p>
                 <p><NavLink to={'/login'}>Try logging in</NavLink></p>
             </Card>
