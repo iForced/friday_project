a340d68037167f38d2718bf756ac54457a66aa00
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2021-11-14 13:45:40 +0300

    сделал случайно revert всех своих коммитов, возвращаю все назад;

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,14 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import {reducer} from "./reducer";
 import thunk from "redux-thunk";
+import {regReducer} from "./registration/registration_reducer";
+
 
 const rootReducer = combineReducers({
     reducer,
+    registration: regReducer,
 })
+
+export type RootStateType = ReturnType<typeof rootReducer>
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
