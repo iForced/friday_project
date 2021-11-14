61797b88c47f8df281cb6946d732ed743f3c4d9e
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2021-11-14 13:35:04 +0300

    Revert "add type for rootReducer;"
    This reverts commit f38849af0a6c77ba4ee3feea9ee11f657270fc2d.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,9 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import {reducer} from "./reducer";
 import thunk from "redux-thunk";
-import {regReducer} from "./registration/registration_reducer";
-
 
 const rootReducer = combineReducers({
     reducer,
-    regReducer,
 })
-
-export type RootStateType = ReturnType<typeof rootReducer>
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
