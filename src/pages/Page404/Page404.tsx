3c35f2f53d0cf28fef46389f314e2334626296cd
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2021-11-14 14:59:25 +0300

    page 404; header refactor;

--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import s from './Page404.module.css';
+import {Button} from "antd";
+
 
 const Page404 = () => {
+    let navigate = useNavigate();
+
     return (
-        <div>
-            Page 404
+        <div className={s.wrapper}>
+            <h2>404: Page not found <i>😕</i></h2>
+            <Button
+                onClick={() => navigate(-1)}
+                shape={'round'}
+                size={"large"}
+                type={'primary'}
+            >Back
+            </Button>
         </div>
     );
 };
+
 
 export default Page404;
