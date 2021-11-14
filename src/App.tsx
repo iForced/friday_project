8fdba7dece766cb02c2a3f753c54e9d65ca9f506
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2021-11-14 13:35:01 +0300

    Revert "Header component;"
    This reverts commit 779a7c787f765e5ea9736343902bf4a2cd6bf6d2.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import s from './App.module.css'
+import Test from "./pages/Test/Test";
+import {NavLink, Route, Routes} from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Registration from "./pages/Registration/Registration";
 import EnterNewPassword from "./pages/EnterNewPassword/EnterNewPassword";
 import PasswordRecovery from "./pages/PasswordRecovery/PasswordRecovery";
 import Profile from "./pages/Profile/Profile";
 import Page404 from "./pages/Page404/Page404";
-import Header from "./components/Header/Header";
-
 
 const App = () => {
     return (
         <div>
-            <Header/>
+            <nav className={s.navbar}>
+                test commit
+                <NavLink to={'login'}>Login</NavLink>
+                <NavLink to={'registration'}>Registration</NavLink>
+                <NavLink to={'enterpassword'}>Enter password</NavLink>
+                <NavLink to={'recoverypassword'}>Recovery password</NavLink>
+                <NavLink to={'profile'}>Profile</NavLink>
+                <NavLink to={'404'}>404</NavLink>
+            </nav>
             <Routes>
-                <Route path={'/login'} element={<Login/>}/>
-                <Route path={'/registration'} element={<Registration/>}/>
-                <Route path={'/enterpassword'} element={<EnterNewPassword/>}/>
-                <Route path={'/recoverypassword'} element={<PasswordRecovery/>}/>
-                <Route path={'/profile'} element={<Profile/>}/>
-                <Route path={'/*'} element={<Page404/>}/>
+                <Route path={'login'} element={<Login/>}/>
+                <Route path={'registration'} element={<Registration/>}/>
+                <Route path={'enterpassword'} element={<EnterNewPassword/>}/>
+                <Route path={'recoverypassword'} element={<PasswordRecovery/>}/>
+                <Route path={'profile'} element={<Profile/>}/>
+                <Route path={'404'} element={<Page404/>}/>
             </Routes>
+            <Test/>
         </div>
     );
 };
